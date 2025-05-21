a116be785e63ac716b1e9f80020464a66b290452
carloswps <carloswendell62@gmail.com>
2025-05-21 20:50:33 -0300

    Save method adding

--- src/main/java/carloswps/gihut/imageliteapi/aplication/images/ImagesController.java
+++ src/main/java/carloswps/gihut/imageliteapi/aplication/images/ImagesController.java
@@ -1,6 +1,11 @@
 package carloswps.gihut.imageliteapi.aplication.images;
 
+import carloswps.gihut.imageliteapi.domain.entity.Image;
+import carloswps.gihut.imageliteapi.domain.enums.ImageExtension;
+import carloswps.gihut.imageliteapi.domain.services.ImageServices;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,21 +13,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/images")
 @Slf4j
+@RequiredArgsConstructor
 public class ImagesController {
+    private final ImageServices service;
+
     @PostMapping
     public ResponseEntity save(
             @RequestParam("file") MultipartFile file,
             @RequestParam("name") String name,
             @RequestParam("tags") List<String> tags
-    ) {
+    ) throws IOException {
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        log.info("Tags recebidas: {}", tags);
-        log.info("Nome recebido: {}", name);
+        log.info("Tags recebidas: {}", file.getContentType());
+        log.info("Tipo de arquivo: {}", MediaType.valueOf(file.getContentType()));
+
+        Image image = Image.builder()
+                .name(name)
+                .tags(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+        service.save(image);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/carloswps/gihut/imageliteapi/domain/enums/ImageExtension.java
+++ src/main/java/carloswps/gihut/imageliteapi/domain/enums/ImageExtension.java
@@ -1,9 +1,25 @@
 package carloswps.gihut.imageliteapi.domain.enums;
 
-public enum ImageExtension {
-    JPG,
-    PNG,
-    GIF,
-    JPEG;
+import org.springframework.http.MediaType;
 
+import java.util.Arrays;
+
+public enum ImageExtension {
+    PNG(MediaType.IMAGE_PNG),
+    GIF(MediaType.IMAGE_GIF),
+    JPEG(MediaType.IMAGE_JPEG);
+
+    private MediaType mediaType;
+
+    private ImageExtension(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public static ImageExtension valueOf(MediaType mediaType) {
+        return Arrays
+                .stream(values())
+                .filter(e -> e.mediaType.equals(mediaType))
+                .findFirst()
+                .orElse(null);
+    }
 }
